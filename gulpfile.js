73c074453fa48f317fe8fcdb068b05308b5811b8
Maria Druzina <maria.druzina@opensoftdev.ru>
2015-08-20 10:01:57 +0300

    Gulp: adding task for cleaning libs path

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,20 @@
 var gulp = require('gulp');
 var bower = require('gulp-bower');
+var clean = require('gulp-clean');
 
-gulp.task('bower', function() {
+gulp.task('clean-libs', function() {
+    return gulp
+        .src('src/lib/*', {
+            read: false
+        })
+        .pipe(clean());
+});
+
+gulp.task('bower', ['clean-libs'], function() {
     return bower()
         .pipe(gulp.dest('src/lib'));
 });
 
-gulp.task('default', ['bower']);
+gulp.task('install-libs', ['bower']);
+
+gulp.task('default', ['install-libs']);
