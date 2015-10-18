281f8870bdb26f5f2f0462df668089805e540ea3
Maria Druzina <druzina.mary@gmail.com>
2015-10-18 13:33:56 +0300

    Got rid of requirejs

--- src/app/map/layers.service.js
+++ src/app/map/layers.service.js
@@ -1,31 +1,31 @@
-define([], function () {
-    'use strict';
+'use strict';
 
-    LayersDataService.$inject = ['$http'];
+angular
+    .module('sfMap')
+    .factory('sfLayersDataService', LayersDataService);
 
-    function LayersDataService($http) {
-        var factory = {
-            getLayers: getLayers
-        };
+LayersDataService.$inject = ['$http'];
 
-        return factory;
+function LayersDataService($http) {
+    var factory = {
+        getLayers: getLayers
+    };
 
-        ///////////////
+    return factory;
 
-        function getLayers() {
-            return $http.get('http://openmuseum.azurewebsites.net/api/Layers')
-                .then(getLayersComplete)
-                .catch(getLayersFailed);
+    ///////////////
 
-            function getLayersComplete(response) {
-                return response.data;
-            }
+    function getLayers() {
+        return $http.get('http://openmuseum.azurewebsites.net/api/Layers')
+            .then(getLayersComplete)
+            .catch(getLayersFailed);
 
-            function getLayersFailed(error) {
-                //logger.error('XHR Failed for getMaps: ' + error.data);
-            }
+        function getLayersComplete(response) {
+            return response.data;
+        }
+
+        function getLayersFailed(error) {
+            //logger.error('XHR Failed for getMaps: ' + error.data);
         }
     }
-
-    return LayersDataService;
-});
+}
